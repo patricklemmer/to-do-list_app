5e31369ce7616300252578cf6beec5fea8de3398
meerkatstudio <patrick.lemmer@yahoo.com>
2022-04-16 22:16:59 +0200

    added functionality of adding items

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 function newItem() {
+  //Adds a new item to the list of items
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
+  if (inputValue === ' ') {
+    alert('You must write something!');
+  } else {
+    $('#list').append(li);
+  }
 }
